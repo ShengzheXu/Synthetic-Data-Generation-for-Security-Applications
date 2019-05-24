1. Analyze more users 2. correct the integrate for baseline2 3. Likelihood computation for baseline2 4. [WIP] more detail laplacian smoothing for baseline2 to be fixed.

models/baselines.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 import operator
 import configparser
+from scipy import integrate
 
 np.random.seed(131)
 
@@ -176,14 +177,14 @@ class baseline2(baseline):
         super().__init__()
         self.byt_model = None
     
-    def fit(self, start_date, sip, byt_log_train, time_delta_train, sip_train, dip_train, byt1_log_train, teT_col, bins):
+    def fit(self, start_date, sip, byt_log_train, time_delta_train, sip_train, dip_train, byt1_log_train, teT_train, the_df, bins):
         super().fit(sip, start_date, time_delta_train, sip_train, dip_train)
         
         self.bins = bins
         self.byt_model = mixture.GaussianMixture(n_components=7, covariance_type='full').fit(byt_log_train)
-        self.learnt_p_B_gv_T_B1 = self.build_bayesian_dep(teT_col, np.ravel(byt_log_train), np.ravel(byt1_log_train), self.byt_model)
+        self.learnt_p_B_gv_T_B1 = self.build_bayesian_dep(the_df, np.ravel(byt_log_train), np.ravel(byt1_log_train), self.byt_model)
 
-        self.cal_likelihood(byt_log_train, byt1_log_train, teT_col)
+        self.cal_likelihood(byt_log_train, byt1_log_train, teT_train)
 
     def cal_likelihood(self, given_data_byt, given_data_byt_1, given_data_T):
         # learnt_p_B_gv_T_B1[t][interval_1][interval]
@@ -196,7 +197,7 @@ class baseline2(baseline):
             add_likeli += np.log(self.learnt_p_B_gv_T_B1[id_t][id_byt_1][id_byt])
         
         log_likeli = add_likeli/len(given_data_byt)
-        print('likelihood:', add_likeli, add_likeli/len(given_data_byt))
+        print('likelihood:', add_likeli, log_likeli)
         self.likelihood = log_likeli
     
     def generate_one(self, dep_info):
@@ -213,12 +214,12 @@ class baseline2(baseline):
         # print(type(self.bins))
         return pd.cut([byt_number], self.bins)[0]
 
-    def integrate(self, f, a, b, N):
-        x = np.linspace(a+(b-a)/(2*N), b-(b-a)/(2*N), N)
-        x = np.reshape(x, (-1, 1))
-        fx = f(x)
-        area = np.sum(fx)*(b-a)/N
-        return area
+    # def integrate(self, f, a, b, N):
+    #     x = np.linspace(a+(b-a)/(2*N), b-(b-a)/(2*N), N)
+    #     x = np.reshape(x, (-1, 1))
+    #     fx = f(x)
+    #     area = np.sum(fx)*(b-a)/N
+    #     return area
 
     def build_bayesian_dep(self, all_record, log_byt_col, log_byt_1_col, clf):
         print(self.bins)
@@ -244,7 +245,8 @@ class baseline2(baseline):
 
         for interval in bins_name:
             print('now working on:', interval)
-            p_B[interval] = self.integrate(clf.score_samples, interval.left, interval.right, 100)
+            gmm_pdf = lambda x: clf.score_samples(np.reshape([x], (-1, 1)))
+            p_B[interval], _est_error = integrate.quad(gmm_pdf, interval.left, interval.right)
             df = all_record[all_record['bytBins'] == interval]
 
             for t in range(24):
@@ -263,6 +265,7 @@ class baseline2(baseline):
                 learnt_p_B_gv_T_B1[t][interval_1] = {}
                 for interval in bins_name:
                     learnt_p_B_gv_T_B1[t][interval_1][interval] = p_T_B[t][interval] * p_B[interval] * p_B1_B[interval_1][interval]
+                    print(t, interval_1, interval, p_T_B[t][interval], p_B[interval], p_B1_B[interval_1][interval])
 
         # do a laplacian smoothing for the learnt table
         from utils.distribution_utils import get_distribution_with_laplace_smoothing
@@ -279,6 +282,7 @@ class baseline2(baseline):
                 for interval in bins_name:
                     learnt_p_B_gv_T_B1[t][interval_1][interval] = smoothed_list[ith]
                     ith += 1
+                    # print(t, interval_1, learnt_p_B_gv_T_B1[t][interval_1])
                 
         return learnt_p_B_gv_T_B1
 

run_exp.py
@@ -40,18 +40,20 @@ def data_prepare(ip_str, sample_flag):
     sip_train_pool = np.ravel(all_record['sa'].values)
     dip_train_pool = np.ravel(all_record['da'].values)
 
-    teT_df_col = all_record#['teT']
+    teT_train = np.reshape(all_record['teDelta'].values, (-1, 1))
+    df_col = all_record#['teT']
+    
 
-    return byt_log_train, time_delta_train, sip, sip_train_pool, dip_train_pool, byt1_log_train, teT_df_col
+    return byt_log_train, time_delta_train, sip, sip_train_pool, dip_train_pool, byt1_log_train, teT_train, df_col
 
-def model_prepare(original_date, sip, byt_log_train, time_delta_train, sip_train, dip_train, byt1_log_train=None, teT_df_col=None):
+def model_prepare(original_date, sip, byt_log_train, time_delta_train, sip_train, dip_train, byt1_log_train=None, final_teT_train=None, teT_df_col=None):
     if baseline_choice == 'baseline1':
         meta_model = baseline1()
         meta_model.fit(original_date, sip, byt_log_train, time_delta_train, sip_train, dip_train)
         return meta_model
     elif baseline_choice == 'baseline2':
         meta_model = baseline2()
-        meta_model.fit(original_date, sip, byt_log_train, time_delta_train, sip_train, dip_train, byt1_log_train, teT_df_col, bins)
+        meta_model.fit(original_date, sip, byt_log_train, time_delta_train, sip_train, dip_train, byt1_log_train, final_teT_train, teT_df_col, bins)
         return meta_model
     else:
         pass
@@ -78,12 +80,13 @@ def train_model(baseline_choice):
     final_sip_train = np.ravel(np.array([]))
     final_dip_train = np.ravel(np.array([]))
     final_byt1_log_train = np.reshape(np.array([]), (-1, 1))
+    final_teT_train = np.reshape(np.array([]), (-1, 1))
     final_teT_df_col = None
 
 
     for deal_str in user_list:
         print('loading:' + deal_str)
-        byt_log_train, time_delta_train, sip, sip_train_pool, dip_train_pool, byt1_log_train, teT_df_col = data_prepare(deal_str, sample_flag)
+        byt_log_train, time_delta_train, sip, sip_train_pool, dip_train_pool, byt1_log_train, teT_train, all_data_col = data_prepare(deal_str, sample_flag)
 
         final_byt_log_train = np.concatenate((final_byt_log_train, byt_log_train))
         final_time_delta_train = np.concatenate((final_time_delta_train, time_delta_train))
@@ -92,16 +95,17 @@ def train_model(baseline_choice):
         final_dip_train = np.concatenate((final_dip_train, dip_train_pool))
 
         final_byt1_log_train = np.concatenate((final_byt1_log_train, byt1_log_train))
+        final_teT_train = np.concatenate((final_teT_train, teT_train))
         if final_teT_df_col is None:
-            final_teT_df_col = teT_df_col
+            final_teT_df_col = all_data_col
         else:
-            final_teT_df_col = final_teT_df_col.append(teT_df_col) #np.concatenate((final_teT_df_col, teT_df_col)) #
+            final_teT_df_col = final_teT_df_col.append(all_data_col) #np.concatenate((final_teT_df_col, teT_df_col)) #
         # print(len(final_byt_log_train))
-        del byt_log_train, time_delta_train, sip, sip_train_pool, dip_train_pool, byt1_log_train, teT_df_col
+        del byt_log_train, time_delta_train, sip, sip_train_pool, dip_train_pool, byt1_log_train, teT_train, all_data_col
         gc.collect()
 
-    print(final_teT_df_col.shape)
-    model1 = model_prepare(original_date, final_sip, final_byt_log_train, final_time_delta_train, final_sip_train, final_dip_train, final_byt1_log_train, final_teT_df_col)
+    print("teT::", type(final_teT_df_col), final_teT_df_col.shape)
+    model1 = model_prepare(original_date, final_sip, final_byt_log_train, final_time_delta_train, final_sip_train, final_dip_train, final_byt1_log_train, final_teT_train, final_teT_df_col)
     print(model1.likelihood)
     return model1
 

show_data.py
@@ -3,6 +3,7 @@ import configparser
 import pandas as pd
 import glob
 import utils.plot_utils as plot_utils
+import numpy as np
 
 if __name__ == "__main__":
     print(len(sys.argv))
@@ -14,26 +15,20 @@ if __name__ == "__main__":
     if '-raw' in sys.argv:
         print('reach show raw_data')
         
-        config = configparser.ConfigParser()
-        config.read('config.ini')
-        user_list = config['DEFAULT']['userlist'].split(',')
-        
-        x_data = []
-        y_data = []
-        for ip in user_list:
-            x_data.append(ip)
-            y_data_i = []
-            source_data = './data/raw_data/day_1_%s.csv' % ip
-            all_record = pd.read_csv(source_data)
+        source_folder = './data/midium100users/raw_data/'
+        # source_folder = './data/gen_data/baseline2_1days_folder/'
 
-            for t_hour in range(24):
-                str_hour = str(t_hour) if t_hour > 9 else '0'+ str(t_hour)
-                chunk = all_record[all_record['te'].str.contains(' '+str_hour+':')]
-                y_data_i.append(len(chunk.index))
-            
-            y_data.append(y_data_i)
+        all_record = pd.concat([pd.read_csv(f) for f in glob.glob(source_folder+'*.csv')], ignore_index = True)
+
+        x_data = [source_folder]
+        y_data = [[]]
+        for t_hour in range(24):   
+            y_data_i = []
+            str_hour = str(t_hour) if t_hour > 9 else '0'+ str(t_hour)
+            chunk = all_record[all_record['te'].str.contains(' '+str_hour+':')]
+            y_data[0].append(len(chunk.index))
         
-        plot_utils.temporal_lineplot(x_data, y_data)
+        plot_utils.temporal_lineplot(x_data, y_data, x_label="hour", y_label="#flow", title="#flow distribution over 100 users")
 
     if '-spe' in sys.argv:
         # testing_name = input("Input the testing file name:")
@@ -42,8 +37,8 @@ if __name__ == "__main__":
         # source_data = './data/raw_data/%s.csv' % testing_name
         # all_record = pd.read_csv(source_data)
 
-        source_folder = './data/raw_data/'
-        source_folder = './data/gen_data/baseline2_1days_folder/'
+        source_folder = './data/midium100users/raw_data/'
+        # source_folder = './data/gen_data/baseline2_1days_folder/'
 
         all_record = pd.concat([pd.read_csv(f) for f in glob.glob(source_folder+'*.csv')], ignore_index = True)
 
@@ -55,10 +50,12 @@ if __name__ == "__main__":
             y_data_i = []
             str_hour = str(t_hour) if t_hour > 9 else '0'+ str(t_hour)
             chunk = all_record[all_record['te'].str.contains(' '+str_hour+':')]
-            y_data_i = chunk['byt'].values.tolist()
+            y_data_i = np.log(chunk['byt'].values.tolist())
+            # y_data_i = chunk['byt'].values.tolist()
+            print(t_hour, len(y_data_i), y_data_i)
             y_data.append(y_data_i)
         
         # plot_utils.temporal_lineplot(x_data, y_data)
-        plot_utils.boxplot(x_data, y_data, x_label='hour', y_label='byt', title='hour distribution of all '+source_folder)
+        plot_utils.boxplot(x_data, y_data, x_label='hour', y_label='byt (log)', title='hour distribution of all '+source_folder)
 
     